6b4444ee653f34936c0432620216bcc5accce7e8
Archana Kumari <83866516+arc2001@users.noreply.github.com>
2023-06-21 09:31:42 +0530

    Update Dice.js

--- Dice.js
+++ Dice.js
@@ -1,19 +1,30 @@
-var randomNumber1=Math.floor(Math.random()*6)+1;
 
 
-var ranndomDiceImage="dice"+randomNumber1+".png";
-var randomImageSource="C:/Users/archa/OneDrive/Documents/Dicee Challenge - Starting Files/images/"+ranndomDiceImage;
-var image1=document.querySelectorAll("img")[0];
-image1.setAttribute("src",randomImageSource);
-var randomNumber2=Math.floor(Math.random()*6)+1;
-var randomImageSource2="dice"+randomNumber2+".png";
-document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
-if(randomNumber>randomNumber2){
-    document.querySelector("h1").innerHTML="player1"
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "Dicee Challenge - Starting Files/images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "Dicee Challenge - Starting Files/images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
 }
-else if(randomNumber<randomNumber2){
-    document.querySelector("h1").innerHTML="player2"
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
-else{
-    document.querySelector("h1").innerHTML="draw"
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
 }
